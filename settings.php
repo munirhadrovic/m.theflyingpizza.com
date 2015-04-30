4537f056ea4eb0b72def325c6f9eea79a2ea4067
Emir <emiratesbp@gmail.com>
2015-04-30 13:43:31 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,35 @@
+<?php
+	$adresa[0]="223 North Main Street"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Downtown Dayton, OH";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="The Original Flying Pizza";
+	$telephone[0]=9372228031;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="2640 Colonel Glenn Highway"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Fairborn, Ohio";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Beavercreek / Fairborn";
+	$telephone[1]=9374264688;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="421 Miamisburg-Centerville Rd."; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Centerville, Ohio";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Centerville";
+	$telephone[2]=9374391234;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="5216 Bethel Centere Mall Rd."; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Columbus, Ohio";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="Bethel";
+	$telephone[3]=6144572323;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="9563 Fields Ertel Road"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="Marco";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]="Mason, OH"; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="Mason / Montgomery";
+	$telephone[4]=5136838448;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=5;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
